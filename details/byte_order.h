f45a3da314f9992a85ea517922752baf36c014d0
newenclave <pikrat.acid@gmail.com>
2017-04-04 19:34:09 +0300

    etool

--- details/byte_order.h
+++ details/byte_order.h
@@ -152,8 +152,8 @@ namespace etool { namespace details {
         typedef std::uint16_t value_type;
         static value_type value( value_type v )
         {
-            return ( ( v & 0xff00 ) >> 8 ) |
-                   ( ( v & 0x00ff ) << 8 ) ;
+            return ( ( v & 0xFF00 ) >> 8 ) |
+                   ( ( v & 0x00FF ) << 8 ) ;
         }
 
         template <typename U>
@@ -182,10 +182,10 @@ namespace etool { namespace details {
         typedef std::uint32_t value_type;
         static value_type value( value_type v )
         {
-            return ( ( v & 0xff000000 ) >> 24 ) |
-                   ( ( v & 0x00ff0000 ) >>  8 ) |
-                   ( ( v & 0x0000ff00 ) <<  8 ) |
-                   ( ( v & 0x000000ff ) << 24 ) ;
+            return ( ( v & 0xFF000000 ) >> 24 ) |
+                   ( ( v & 0x00FF0000 ) >>  8 ) |
+                   ( ( v & 0x0000FF00 ) <<  8 ) |
+                   ( ( v & 0x000000FF ) << 24 ) ;
         }
 
         template <typename U>
@@ -218,14 +218,14 @@ namespace etool { namespace details {
         typedef std::uint64_t value_type;
         static value_type value( value_type v )
         {
-            return ( ( v & 0xff00000000000000 ) >> 56) |
-                   ( ( v & 0x00ff000000000000 ) >> 40) |
-                   ( ( v & 0x0000ff0000000000 ) >> 24) |
-                   ( ( v & 0x000000ff00000000 ) >>  8) |
-                   ( ( v & 0x00000000ff000000 ) <<  8) |
-                   ( ( v & 0x0000000000ff0000 ) << 24) |
-                   ( ( v & 0x000000000000ff00 ) << 40) |
-                   ( ( v & 0x00000000000000ff ) << 56) ;
+            return ( ( v & 0xFF00000000000000 ) >> 56) |
+                   ( ( v & 0x00FF000000000000 ) >> 40) |
+                   ( ( v & 0x0000FF0000000000 ) >> 24) |
+                   ( ( v & 0x000000FF00000000 ) >>  8) |
+                   ( ( v & 0x00000000FF000000 ) <<  8) |
+                   ( ( v & 0x0000000000FF0000 ) << 24) |
+                   ( ( v & 0x000000000000FF00 ) << 40) |
+                   ( ( v & 0x00000000000000FF ) << 56) ;
         }
 
         template <typename U>
