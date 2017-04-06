ab8a1bdf2906de45077176c7f66c0b2794a13827
newenclave <pikrat.acid@gmail.com>
2017-04-06 16:54:56 +0300

    etool

--- sizepack/blockchain_varint.h
+++ sizepack/blockchain_varint.h
@@ -11,17 +11,19 @@ namespace etool { namespace sizepack {
 
         typedef std::uint64_t size_type;
 
-        static const size_t max_length = sizeof(size_type) + 1;
-        static const size_t min_length = 1;
+        static const size_t min_length = sizeof(std::uint8_t);
+        static const size_t max_length = sizeof(size_type) + min_length;
 
         using u16_little = details::byte_order_little<std::uint16_t>;
         using u32_little = details::byte_order_little<std::uint32_t>;
         using u64_little = details::byte_order_little<std::uint64_t>;
 
         enum prefix_value {
-            PREFIX_VARINT16 = 0xFD,
-            PREFIX_VARINT32 = 0xFE,
-            PREFIX_VARINT64 = 0xFF,
+            PREFIX_VARINT_MIN = 0xFD,
+            PREFIX_VARINT16   = 0xFD,
+            PREFIX_VARINT32   = 0xFE,
+            PREFIX_VARINT64   = 0xFF,
+            PREFIX_VARINT_MAX = 0xFF,
         };
 
         static
@@ -29,11 +31,11 @@ namespace etool { namespace sizepack {
         {
             switch (prefix) {
             case PREFIX_VARINT64:
-                return sizeof(std::uint64_t) + 1;
+                return sizeof(std::uint64_t) + sizeof(std::uint8_t);
             case PREFIX_VARINT32:
-                return sizeof(std::uint32_t) + 1;
+                return sizeof(std::uint32_t) + sizeof(std::uint8_t);
             case PREFIX_VARINT16:
-                return sizeof(std::uint16_t) + 1;
+                return sizeof(std::uint16_t) + sizeof(std::uint8_t);
             default:
                 break;
             }
@@ -65,14 +67,14 @@ namespace etool { namespace sizepack {
         static
         size_t result_length( size_type len )
         {
-            if( len < 0xFD ) {
+            if( len < PREFIX_VARINT_MIN ) {
                 return sizeof(std::uint8_t);
             } else if( len <= 0xFFFF ) {
-                return sizeof(std::uint16_t) + 1;
+                return sizeof(std::uint16_t) + sizeof(std::uint8_t);
             } else if( len <= 0xFFFFFFFF ) {
-                return sizeof(std::uint32_t) + 1;
+                return sizeof(std::uint32_t) + sizeof(std::uint8_t);
             } else {
-                return sizeof(std::uint64_t) + 1;
+                return sizeof(std::uint64_t) + sizeof(std::uint8_t);
             }
         }
 
@@ -80,18 +82,21 @@ namespace etool { namespace sizepack {
         size_t pack( size_type size, void *result )
         {
             std::uint8_t *res  = reinterpret_cast<std::uint8_t *>(result);
-            if( size < 0xFD ) {
+            if( size < PREFIX_VARINT_MIN ) {
                 res[0] = static_cast<std::uint8_t>(size);
-                return 1;
+                return sizeof(std::uint8_t);
             } else if( size <= 0xFFFF ) {
                 res[0] = static_cast<std::uint8_t>(PREFIX_VARINT16);
-                return u16_little::write( size, &res[1] ) + 1;
+                return u16_little::write( size, &res[1] ) +
+                        sizeof(std::uint8_t);
             } else if( size <= 0xFFFFFFFF ) {
                 res[0] = static_cast<std::uint8_t>(PREFIX_VARINT32);
-                return u32_little::write( size, &res[1] ) + 1;
+                return u32_little::write( size, &res[1] ) +
+                        sizeof(std::uint8_t);
             } else {
                 res[0] = static_cast<std::uint8_t>(PREFIX_VARINT64);
-                return u64_little::write( size, &res[1] ) + 1;
+                return u64_little::write( size, &res[1] ) +
+                        sizeof(std::uint8_t);
             }
         }
 
@@ -150,18 +155,18 @@ namespace etool { namespace sizepack {
 
             size_type res_ = 0;
 
-            if( len == 1 ) {
+            if( len == sizeof(std::uint8_t) ) {
                 res_ = static_cast<size_type>(*d);
              } else {
                 switch (*d) {
                 case PREFIX_VARINT16:
-                    res_ = u16_little::read( d + 1 );
+                    res_ = u16_little::read( d + sizeof(std::uint8_t) );
                     break;
                 case PREFIX_VARINT32:
-                    res_ = u32_little::read( d + 1 );
+                    res_ = u32_little::read( d + sizeof(std::uint8_t) );
                     break;
                 case PREFIX_VARINT64:
-                    res_ = u64_little::read( d + 1 );
+                    res_ = u64_little::read( d + sizeof(std::uint8_t) );
                     break;
                 default:
                     break;
