b8b1eed8873b2e0640194edf6815fc01f29c52d8
newenclave <pikrat.acid@gmail.com>
2017-08-04 13:24:27 +0300

    value class

--- include/etool/intervals/value.h
+++ include/etool/intervals/value.h
@@ -2,6 +2,7 @@
 #define ETOOL_INTERVALS_TRAITS_INFINITE_H
 
 #include <functional>
+#include <ostream>
 #include "etool/intervals/attributes.h"
 #include "etool/intervals/operators.h"
 
@@ -11,9 +12,14 @@ namespace etool { namespace intervals {
     class value {
 
         using domain_type = DomainT;
-        using cmp = operators::cmp<domain_type, Comparator>;
 
     public:
+
+        template <typename ...Args>
+        value( attributes a, Args&& ...args )
+            :val_(std::forward<Args>(args)...)
+            ,attr_(a)
+        { }
 
         attributes attr( ) const
         {
@@ -25,10 +31,82 @@ namespace etool { namespace intervals {
             return val_;
         }
 
+        template <typename ...Args>
+        static
+        value make( Args&& ...args )
+        {
+            return value(attributes::OPEN, std::forward<Args>(args)...);
+        }
+
+        template <typename ...Args>
+        static
+        value make( attributes a, Args&& ...args )
+        {
+            return value( a, std::forward<Args>(args)... );
+        }
+
+        template <typename ...Args>
+        static
+        value make_max_inf( Args&& ...args )
+        {
+            return value(attributes::MAX_INF, std::forward<Args>(args)...);
+        }
+
+        template <typename ...Args>
+        static
+        value make_min_inf( Args&& ...args )
+        {
+            return value(attributes::MIN_INF, std::forward<Args>(args)...);
+        }
+
+        struct cmp {
+            using ocmp = operators::cmp<domain_type, Comparator>;
+
+            static
+            bool less( const value &lh, const value &rh )
+            {
+                if( rh.attr( ) == attributes::MAX_INF ) {
+                    return (lh.attr( ) != attributes::MAX_INF);
+                } else if( rh.attr( ) == attributes::MIN_INF ) {
+                    return false;
+                }
+
+                switch(lh.attr( )) {
+                case attributes::MAX_INF:
+                    return false;
+                case attributes::MIN_INF:
+                    return rh.attr( ) != attributes::MIN_INF;
+                case attributes::OPEN:
+
+                case attributes::CLOSE:
+                    break;
+                }
+                return false;
+            }
+        };
+
     private:
         domain_type val_;
         attributes  attr_;
     };
+
+    template <typename D, typename C>
+    inline
+    std::ostream &operator << ( std::ostream &o, const value<D, C> &v )
+    {
+        switch (v.attr( )) {
+        case attributes::MAX_INF:
+            o << "inf";
+            break;
+        case attributes::MIN_INF:
+            o << "-inf";
+            break;
+        case attributes::OPEN:
+        case attributes::CLOSE:
+            o << v.get( );
+        }
+        return o;
+    }
 
 }}
 
