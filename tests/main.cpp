2e3633eb108f499ea7f8c62735a0f75d86f9afc8
newenclave <pikrat.acid@gmail.com>
2017-08-09 18:40:54 +0300

    lex cast

--- tests/main.cpp
+++ tests/main.cpp
@@ -10,14 +10,35 @@
 #include "etool/observers/define.h"
 #include "etool/details/type_uid.h"
 #include "etool/details/lexical_cast.h"
+#include "etool/slices/memory.h"
 
 #include "etool/charset/utf8.h"
 
 using namespace etool;
 
+using str_slice = slices::memory<const char>;
+
+void perm (int v[], int n, int i)
+{
+    int j;
+    if (i == n) {
+        for (j=0; j<n; j++) printf ("%d ", v[j]);
+        printf ("\n");
+    } else {
+        for (j=i; j<n; j++) {
+            std::swap (v[i], v[j]);
+            perm(v, n, i + 1);
+            std::swap (v[i], v[j]);
+        }
+    }
+}
+
 int main(  int /*argc*/, char** /*argv[ ]*/ )
 {
-    std::cout << details::lexical_cast<double>(L"-4.667") << "\n";
+    int v[5], i;
+
+    for (i=0; i<5; i++) v[i] = i+1;
+    perm (v, 5, 0);
     return 0;
 }
 
