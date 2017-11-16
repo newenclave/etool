d7e7619e01306403f7fab2a7f4d445c128da20bf
newenclave <pikrat.acid@gmail.com>
2017-11-16 10:36:44 +0200

    Some fixes for observer

---
+++ tests/observers.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "etool/observers/simple.h"
+
+#include "catch.hpp"
+
+using namespace etool;
+
+TEST_CASE("Observers", "[observer]") {
+
+    observers::simple<void()> observer;
+
+    SECTION("simple_call") {
+        int test = 0;
+        auto call1 = [&]() { if (test++ == 0) { observer.unsubscribe_all(); } };
+        auto call2 = [&]() { 
+            if (++test == 2) {
+                observer();
+            };
+        };
+        auto call3 = [&]() { ++test; };
+        observer.connect(call1);
+        observer.connect(call2);
+        observer.connect(call3);
+        observer();
+        REQUIRE(test == 6);
+    }
+}
