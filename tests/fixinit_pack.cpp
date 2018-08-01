5cabaa30538be01f219910deef7cddca886ed298
newenclave <pikrat.acid@gmail.com>
2018-08-01 09:59:45 +0300

    UT

---
+++ tests/fixinit_pack.cpp
@@ -0,0 +1,20 @@
+#include "etool/sizepack/fixint.h"
+
+#include "catch.hpp"
+
+using namespace etool;
+
+TEST_CASE("FixedInteger", "[fixints]")
+{
+	SECTION("pack unpack")
+	{
+		using pack_type = sizepack::fixint<std::uint32_t>;
+		std::uint32_t test_data = 0xC8F681A3;
+		std::string result;
+		pack_type::pack(test_data, result);
+		REQUIRE(result.size() == pack_type::min_length);
+
+		std::uint32_t res_data = pack_type::unpack(result.begin(), result.end());
+		REQUIRE(res_data == test_data);
+	}
+}
